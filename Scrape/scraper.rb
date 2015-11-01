5a5017bc5acc5a7bd130b0bc8fe0e18d0917860b
Dragos Dumitrache <dragos.dumitrache13@imperial.ac.uk>
2015-11-01 22:55:03 +0000

    Marie Curie Wiki example

--- Scrape/scraper.rb
+++ Scrape/scraper.rb
@@ -7,4 +7,20 @@ require 'net/http'
 
 def main
   agent = Mechanize.new
+  base_wikipedia_url = "https://en.wikipedia.org"
+  list_url = "#{base_wikipedia_url}/wiki/Marie_Curie";
+  page = agent.get(list_url)
+  title = page.parser.xpath('//h1').text
+  content = ""
+  content.concat(title)
+  content.concat("\n")
+  page.parser.xpath('//p').each do |info|
+    content.concat(info.text)
+    content.concat("\n")
+  end
+  File.open(title + ".txt", 'w') do |f|
+    f << content
+  end
 end
+
+main
