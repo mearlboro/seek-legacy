a0f43e3cdc07efd10d97d6e063cbe360078b02d8
ms6413 <ms6413@ic.ac.uk>
2015-11-08 00:01:14 +0000

    IMPORTANT: Removed seek-env and added it to gitignore. Everyone should run readme.sh when cloning.

--- seek-env/bin/runxlrd.py
+++
@@ -1,5 +0,0 @@
-#!/home/mada/Hacking/seek/seek-env/bin/python3
-# EASY-INSTALL-SCRIPT: 'xlrd==0.9.4','runxlrd.py'
-__requires__ = 'xlrd==0.9.4'
-import pkg_resources
-pkg_resources.run_script('xlrd==0.9.4', 'runxlrd.py')
