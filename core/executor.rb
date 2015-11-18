935890ffdd6da5783cc8a445cd13dc174470ad87
root <root@new-temp>
2015-11-18 20:15:47 +0000

    updated gitignore to don't care about test created files. 100% test coverage on both executor and extractor :)

--- core/executor.rb
+++ core/executor.rb
@@ -1,24 +1,27 @@
 #!/usr/bin/env ruby
 
 # Parse arguments at input level, make difference between file and directory
-files = sys.argv[1]
-dst = Dir.pwd + '/txt'
+files = ARGV[0]
+dst   = ARGV[1]
 
 if File.file?(files)
-	system("python extractor.py #{files} #{dst}")
-end
-queue = Queue.new
-files.map { |file| queue << file }
-thread_count = 5
-threads = thread_count.times.map do
-  Thread.new do
-    while !queue.empty?
-      elem = queue.pop
-      system("python extractor.py #{elem} #{dst}")
+    system("python extractor.py #{files} #{dst}")
+else
+    queue = Queue.new
+    allFiles = Dir["#{files}/**/*"]
+    allFiles = allFiles.select { |file| File.file?(file) }
+    allFiles.map { |file| queue << file }
+    thread_count = 5
+    threads = thread_count.times.map do
+    Thread.new do
+        while !queue.empty?
+            elem = queue.pop
+            system("python extractor.py #{elem} #{dst}")
+        end
     end
-  end
-end
+    end
 threads.each(&:join)
+end
 # files.each_slice(100) do |batch|
 #     threads = []
 #     batch.each do |elem|
--- tests/executorTests.rb
+++ tests/executorTests.rb
@@ -5,33 +5,42 @@ require_relative 'testUtils'
 
 class Executor_test < Test::Unit::TestCase
 
-    $executor_command = "ruby #{File.join('../', 'core', 'executor.rb')}"
-    $dst_directory    = File.join('testExtractedData', 'pdf/')
+    $executor_command   = "ruby executor.rb"
+    $dst_directory      = File.join('..', 'tests' ,'testExtractedData', 'pdf/')
+    #Because the executor calls extractor.py, we need to change directory
+    #at the beginning of each test
+    $executor_directory = File.join('..', 'core')
 
     def test_can_take_single_file_as_parameter
+        Dir.chdir $executor_directory
         test_file         = 'extractor_test_basics'
-        src_directory     = File.join('testRawData', 'pdf/')
+        src_directory     = File.join('..', 'tests', 'testRawData', 'pdf/')
 
         system("#{$executor_command}  #{src_directory}#{test_file}.pdf #{$dst_directory}")
-        assert(File.exists?("#{$dst_directory}#{test_file}.txt") , 'Files were not generated')
+        assert(File.exists?("#{$dst_directory}extractor_test_basics.txt") , 'Files were not generated')
 
         clean_directory
     end
 
     def test_can_take_directory_as_parameter
-        src_directory = File.join('testRawData', 'testSingleFolder/')
-        after, total  = extract_files_and_return_count(src_directory)
-
-        assert(total == after, 'Files were not generated')
+        Dir.chdir $executor_directory
+        src_directory = File.join('..', 'tests', 'testRawData', 'testSingleFolder/')
+        before, after, total  = extract_files_and_return_count(src_directory)
+       
+        assert(before < after, 'Files were not generated')
+        assert(total == after, 'not enough files were generated')
 
         clean_directory
     end
 
     def test_does_extract_files_on_subfolders
-        src_directory = File.join('testRawData', 'testSubFolders/') 
-        after, total  = extract_files_and_return_count(src_directory)
+        Dir.chdir $executor_directory
+        src_directory = File.join('..', 'tests', 'testRawData', 'testSubFolders/') 
+        before, after, total  = extract_files_and_return_count(src_directory)
 
-        assert(total == after, 'Files were not generated')
+        puts "before: #{before} after: #{after}"
+        assert(before < after, 'Files were not generated')
+        assert(total == after, 'not enough files were generated')
 
         clean_directory 
     end
@@ -39,6 +48,9 @@ class Executor_test < Test::Unit::TestCase
     # Runs the executor and returns the values to further check wether the
     # right amount of files have been created
     def extract_files_and_return_count(src_directory)
+        Dir.chdir $executor_directory
+
+        
         number_files_before = TestUtils.count_files_directory($dst_directory)
         number_files_src_directory = TestUtils.count_files_directory(src_directory)
 
@@ -47,14 +59,18 @@ class Executor_test < Test::Unit::TestCase
         number_files_after = TestUtils.count_files_directory($dst_directory)
 
         total_correct_amount_of_files = number_files_before + number_files_src_directory
-        return number_files_after, total_correct_amount_of_files
+        return number_files_before, number_files_after, total_correct_amount_of_files
+
+	clean_directory
     end
 
     def clean_directory
-        system("#{$dst_directory}*.txt")
+        Dir.chdir '../tests'
+        system("rm #{$dst_directory}*.txt")
     end
 end
 
 # Deletes all extracted files before running tests
 puts 'Deleting previous test results'
+
 system("rm testExtractedData/pdf/*.txt")
