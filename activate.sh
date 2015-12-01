c1f6b4d29bf4562ad4552d26fddcc283bb42a5b1
Dragos Dumitrache <dragos.dumitrache13@imperial.ac.uk>
2015-12-01 23:38:27 +0000

    Updated install script, added environment activator script

---
+++ activate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/bin/bash -c ". seek-env/bin/activate; exec /bin/bash -i"
+cd seek-env 
--- install.sh
+++ install.sh
@@ -8,18 +8,20 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
   platform='osx'
 fi
 
+
+## -- ruby --------------------------------------------------------------------
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+rvm install 2.2.1
+/bin/bash --login
+rvm use 2.2.1
+sudo gem install mechanize
+
 if [[ $platform == 'linux' ]]; then
   # -- required: curl ----------------------------------------------------------
   sudo apt-get install curl
-  ## -- ruby --------------------------------------------------------------------
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  curl -sSL https://get.rvm.io | bash -s stable --ruby
-  rvm install 2.2.1
-  /bin/bash --login
-  rvm use 2.2.1
-  sudo gem install mechanize
 
-  ## -- python ------------------------------------------------------------------
+    ## -- python ------------------------------------------------------------------
   sudo apt-get install python3
   sudo apt-get install python-virtualenv
   sudo apt-get install python3-pip
@@ -52,8 +54,18 @@ if [[ $platform == 'linux' ]]; then
   pip3 install -U numpy
   python -m nltk.downloader all
 elif [[ $platform == 'osx' ]]; then
+
+  brew install python3
+
+  # -- setup environment --------------------------------------------------------
+  virtualenv -p /usr/local/bin/python3 seek-env
+  # -- dont forget to always activate / deactivate ------------------------------
+  /bin/bash -c ". seek-env/bin/activate; exec /bin/bash -i"
+  cd seek-env
+
   brew install libxml2 antiword unrtf poppler tesseract flac lame libmad libsoxr libjpeg zlib
   pip3 install --upgrade lxml
+
   curl https://pypi.python.org/packages/source/t/textract/textract-1.4.0.tar.gz > textract-1.4.0.tar.gz
   tar xvf textract-1.4.0.tar.gz
   cd textract-1.4.0
@@ -61,7 +73,7 @@ elif [[ $platform == 'osx' ]]; then
   pip3 install pdfminer3k
   python3 setup.py install
   cd ..
-  ed -s lib/python3.4/site-packages/textract-1.4.0-py3.4.egg/textract/parsers/utils.py <<< $',s/(text, unicode)/(text, str)\nw'
+  ed -s lib/python3.5/site-packages/textract-1.4.0-py3.5.egg/textract/parsers/utils.py <<< $',s/(text, unicode)/(text, str)\nw'
   pip3 install -U gensim
   pip3 install -U nltk
   pip3 install -U numpy
