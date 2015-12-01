8d9b10d110d268d5a36996a8109e2f968b68d2e4
ms6413 <ms6413@ic.ac.uk>
2015-12-01 02:41:28 +0000

    Linguist filters out LDA stop words and gets only the nouns, verbs, adjectives, adverbs and their frequencies

--- core/linguist.py
+++ core/linguist.py
@@ -5,7 +5,7 @@ import os
 import glob
 import sys
 
-# Use script by calling $ python linguist.py <source> <destination>
+# Use script by calling $ python linguist.py <command> <source>
 
 vocab = []
 freqs = nltk.FreqDist('') 
@@ -15,34 +15,76 @@ def gettext(src, filename):
     f = open(src + '/' + filename, 'r+')
     raw_text = f.read()
     toks  = nltk.word_tokenize(raw_text)    # tokenize raw text
-    text  = nltk.Text(toks)               # nltk type text
-    return (toks, text) 
+    text  = nltk.Text(toks)                 # nltk type text
+    return text 
 
+
+# COMMAND vocab
 def getvocab(src):
     for filename in glob.glob(os.path.join(src, '*.txt')):
-        (toks,text)  = gettext(src, filename)
+        text  = gettext(src, filename)
         global vocab
         vocab = sorted(set(vocab + sorted(set([w.lower() for w in text])))) 
-                                                # get vocabulary and add to total vocabulary 
+                                 # get vocabulary and add to total vocabulary 
     print(vocab)
  
+
+# COMMAND freq
 def getfrequency(src):
     for filename in glob.glob(os.path.join(src, '*.txt')):
-        (toks,text)  = gettext(src, filename)
+        text  = gettext(src, filename)
         freq  = nltk.FreqDist(text)
         global freqs 
-        freqs = freqs + freq                    # find frequencies and add to total frequency distribution
+        freqs = freqs + freq     # find frequencies and add to total frequency distribution
 
     print(freqs)
 
+
+# COMMAND ldatokens
 def getldatokens(src):
-    tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())  # this guy here imports a high-end English dictionary. warning: it's just slow, don't ctrl+c 
+    # this guy here imports a high-end English dictionary. warning: it's just slow, don't ctrl+c 
+    tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())
+
+    # nltk.help.upenn_tagset() # to see all
+    filter_pos = set([
+         # 'CD'  , # numeral: cardinal
+        'JJ'  ,  # ordinal adjective or numeral
+        'JJR' ,  # comparative adjective
+        'JJS' ,  # superlative adjective
+        'NN'  ,  # singular or mass common noun
+        'NNS' ,  # plural common noun
+        'NNP' ,  # singular proper noun
+        'NNPS',  # plural proper noun
+        'RB'  ,  # adverb
+        'RBR' ,  # comparative adverb
+        'RBS' ,  # superlative adverb
+        'VB'  ,  # verb    
+        'VBD' ,  # verb past tense
+        'VBG' ,  # verb present participle or gerund
+        'VBN' ,  # verb past participle
+        'VBP' ,  # verb present
+        'VBZ' ,  # verb present 3rd person singular     
+    ])
+
     for filename in glob.glob(os.path.join(src, '*.txt')):
-        (toks,text)  = gettext(src, filename)
+        text = gettext(src, filename)
 
-        # TODO: use the tagger to identify and remove lda stop words
+        # use the tagger to identify and remove lda stop words
         # keep both vocabs and freqs
+        parts_of_speech = nltk.pos_tag(text)
 
+        # filter the good pos
+        lda_parts_of_speech = filter(lambda  pos : pos[1] in filter_pos, parts_of_speech)
+        lda_text = [pos[0].lower() for pos in lda_parts_of_speech]
+
+        global vocab
+        vocab = sorted(set(vocab + lda_text)) 
+                                # get vocabulary and add to total vocabulary 
+        
+        freq  = nltk.FreqDist(lda_text)
+        global freqs
+        freqs = freqs + freq    # find frequencies and add to total frequency distribution
+    
     print(vocab)
     print(freqs)
 
