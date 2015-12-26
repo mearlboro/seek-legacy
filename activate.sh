f2941f35e6332a59689aa525440e1e30d984d86c
mearlboro <ms6413@ic.ac.uk>
2015-12-26 21:32:12 +0200

    Env now created in activate.sh, and added in it gensim and nltk pip installers.

--- activate.sh
+++ activate.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+virtualenv -p /usr/bin/python3 seek-env
+
 /bin/bash -c ". seek-env/bin/activate; exec /bin/bash -i"
 cd seek-env
+
+pip3 install -U gensim
+pip3 install -U nltk
+pip3 install -U numpy
+
