c59a3dc8fb127f36d41bd8bf70f89baef70fde96
Dragos Dumitrache <dragos.dumitrache13@imperial.ac.uk>
2016-01-04 23:30:05 +0000

    Added activator code for nltk_train. Comment out initially. Extracting basic named entities

--- activate.sh
+++ activate.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-virtualenv -p /usr/local/bin/python3 seek-env
+# virtualenv -p /usr/local/bin/python3 seek-env
 
 source seek-env/bin/activate
-# cd seek-env
+
+cd seek-env
+
+git clone https://github.com/japerk/nltk-trainer
+cd nltk-trainer
+python setup.py install
+python train_chunker.py treebank
+python train_tagger.py brown
 
 # pip3 install -U numpy
 # pip3 install -U scipy
