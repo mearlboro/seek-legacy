605ba497746d9be06ac79f4105107ea637332681
Dragos Dumitrache <dragos.dumitrache13@imperial.ac.uk>
2016-01-05 18:02:31 +0000

    BUG in executor.py upload, to be fixed. Added nltk_train & Stanford NER

--- activate.sh
+++ activate.sh
@@ -4,15 +4,26 @@
 
 source seek-env/bin/activate
 
-cd seek-env
+# export NLTK_DATA="$HOME/nltk_data"
+export NLTK_DATA="/usr/local/share/nltk_data"
+cd $NLTK_DATA
 
+# wget http://nlp.stanford.edu/software/stanford-ner-2014-06-16.zip
 git clone https://github.com/japerk/nltk-trainer
 cd nltk-trainer
 python setup.py install
-python train_chunker.py treebank
-python train_tagger.py brown
+# cd seek-env
+export CLASSPATH="$NLTK_DATA/stanford-ner-2014-06-16/stanford-ner.jar"
+export STANFORD_MODELS="$NLTK_DATA/stanford-ner-2014-06-16/classifiers"
+
+# git clone https://github.com/japerk/nltk-trainer
+# cd nltk-trainer
+# python setup.py install
+# python train_chunker.py treebank
+# python train_tagger.py brown
 
 # pip3 install -U numpy
+# pip3 install -U regex
 # pip3 install -U scipy
 # pip3 install -U gensim
 # pip3 install -U nltk
