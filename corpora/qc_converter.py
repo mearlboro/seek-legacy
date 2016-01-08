62d9b989907882edb5c8632873bac03665bb1222
mearlboro <ms6413@ic.ac.uk>
2016-01-08 20:30:35 +0000

    Updated qc_converter.py

--- corpora/qc_converter.py
+++ corpora/qc_converter.py
@@ -6,13 +6,20 @@ to use move in /core
 '''
 
 import nltk
+import json
 from statistician import NameEntityDetector
 
-f = open('qc.txt')
+f = open('../corpora/qc.txt')
 lines = f.readlines()
 toks = list(map(nltk.word_tokenize,lines))
 tuples = [(line[3:],(line[0],line[3])) for line in toks]
+triples = [(' '.join(pair[0]), (pair[0], ['NE'], pair[1])) for pair in tuples]
+dictionary = dict(triples)
 
+# TODO:
 ner = NameEntityDetector()
+
+with open('../corpora/qc.json', 'w') as out:
+    json.dump(dictionary, out)
 
 
