9ae777995421ec2ba6fdf8f59bdfa8ca9275ece2
mearlboro <ms6413@ic.ac.uk>
2016-01-08 09:29:45 +0000

    Fixed bad merge of activate script

--- activate.sh
+++ activate.sh
@@ -5,33 +5,9 @@
 # heavy install.sh script. please comment out duplicate items as some are very
 # resource-intensive and involve ML training
 
-## activates environment
+# activates environment
 source seek-env/bin/activate
 
-# export NLTK_DATA="$HOME/nltk_data"
-export NLTK_DATA="/usr/local/share/nltk_data"
-# cd $NLTK_DATA
-
-# wget http://nlp.stanford.edu/software/stanford-ner-2014-06-16.zip
-# git clone https://github.com/japerk/nltk-trainer
-# cd nltk-trainer
-# python setup.py install
-# cd seek-env
-export CLASSPATH="$NLTK_DATA/stanford-ner-2014-06-16/stanford-ner.jar"
-export STANFORD_MODELS="$NLTK_DATA/stanford-ner-2014-06-16/classifiers"
-
-# git clone https://github.com/japerk/nltk-trainer
-# cd nltk-trainer
-# python setup.py install
-# python train_chunker.py treebank
-# python train_tagger.py brown
-
-# pip3 install -U numpy
-# pip3 install -U regex
-# pip3 install -U scipy
-# pip3 install -U gensim
-# pip3 install -U nltk
-# python -m nltk.downloader all
 ## -- Gensim -------------------------------------------------------------------
 #pip3 install -U numpy
 #pip3 install -U scipy
@@ -42,14 +18,15 @@ export STANFORD_MODELS="$NLTK_DATA/stanford-ner-2014-06-16/classifiers"
 #pip3 install -U nltk
 #python -m nltk.downloader all
 
-# prepare nltk data and download stanford NLP package
-# Path on local machines
+## prepare NLTK data and download stanford NLP package
+## Path on local machines
 # export NLTK_DATA="$HOME/nltk_data"
 ## Path on the server!
 #export NLTK_DATA="/usr/local/share/nltk_data"
 cwd=$(pwd)
 cd $NLTK_DATA
 
+# wget http://nlp.stanford.edu/software/stanford-ner-2014-06-16.zip
 curl http://nlp.stanford.edu/software/stanford-ner-2014-06-16.zip > stanford-ner.zip
 unzip stanford-ner.zip -d stanford-ner
 rm -f stanford-ner.zip
