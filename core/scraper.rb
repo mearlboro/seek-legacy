d183605ecf67dd596b830f5ddf81e79879471fd4
Dragos Dumitrache <dragos.dumitrache13@imperial.ac.uk>
2016-01-16 17:22:15 +0000

    Commented out code

--- core/scraper.rb
+++ core/scraper.rb
@@ -1,10 +1,5 @@
 #!/usr/bin/env ruby
-# begin
-#  gem 'mechanize', ">=2.7"
-# rescue Gem::LoadError => e
-#  system("sudo gem install mechanize")
-#  Gem.clear_paths
-# end
+# Scraper utility tool for data gathering
 
 require 'open-uri'
 require 'mechanize'
@@ -17,9 +12,9 @@ require './scrapy'
 # Defines the number of threads to run Scrapy on
 $thread_count = 5
 
-# Downloads array of files into the target_dir according to the file_names
+# Downloads multiple files into the target_dir according to the file_names
 # Current link is used in the case of relative URIs to create the full path
-def download_files_from_URLs(agent, target_dir, links, override, file_in_names, current_link)
+def download_files_from_URLs(agent, target_dir, links, file_in_names, current_link)
   # Open file from web URL, using username and password provided
 
   uri = URI(current_link)
@@ -29,7 +24,8 @@ def download_files_from_URLs(agent, target_dir, links, override, file_in_names, 
 
   links.map { |url| queue << url }
   file_in_names.map { |name| name_queue << name }
-  # Concurrent file download
+
+  # Concurrent file downloads
   threads = $thread_count.times.map do
     Thread.new do
       Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
@@ -47,28 +43,32 @@ def download_files_from_URLs(agent, target_dir, links, override, file_in_names, 
               name = File.basename(URI.parse(url.to_s).path)
             end
           end
-          # Skip .ps files as no support for PS in textract
+
+          # Skip .ps files as no support for PS exists in textract
           if (File.extname(name) == ".ps")
             next
           end
+
           # Clear name of garbage and add extension
           name = name.gsub(/[\/ ]/, "").gsub(/[%20]/, "_") + ".pdf"
           if (name.size > 100)
             name = name[0..100]
           end
+
           if (File.exists?(name))
             print "Skip, #{name} already exists\n"
           else
             # Perform a new request for the file
             request = Net::HTTP::Get.new(url)
             request.basic_auth(ENV['IC_USERNAME'], ENV['IC_PASSWORD'])
+
             http.request request do |response|
               case response
               when Net::HTTPRedirection
                 # Do Nothing for now
                 # TODO: Fix download of secure files
               when Net::HTTPOK
-                  print "Fetching #{name}\n"
+                  # print "Fetching #{name}\n"
                   # Write chunks of file while also reading new chunks, to increase
                   # efficiency
                   File.open(name, 'w') do |file_out|
@@ -82,6 +82,7 @@ def download_files_from_URLs(agent, target_dir, links, override, file_in_names, 
                 puts "Wrong credentials"
               end
             end
+
           end
         end
       end
@@ -90,6 +91,7 @@ def download_files_from_URLs(agent, target_dir, links, override, file_in_names, 
   threads.each(&:join)
 end   # End of download_file_from_url
 
+# Downloads single file from a URL
 def extract_file_from_url(url, dest)
   request = Net::HTTP::Get.new(url)
   begin
@@ -138,6 +140,7 @@ def parse(agent, args)
       $opts << "-s"
       login = agent.get("https://spiral.imperial.ac.uk/ldap-login")
 
+      # Login for secure downloads
       form = login.form_with(:id => 'loginform')
       username_field = form.field_with(:id => 'tlogin_netid')
       username_field.value = ENV['IC_USERNAME']
